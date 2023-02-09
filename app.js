89af7338715db072bbe23ba7c947ea651b0f8f98
umarbek <umarbekerkinboyev045@gmail.com>
2023-02-09 23:11:11 +0500

    new code

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger-menu'),
 ActiveNavbar = document.querySelector('#mobile-navbar');
+NoTitle = document.querySelector("#nothing");
 
 hamburger.addEventListener('click', (e) => {
     ActiveNavbar.classList.toggle('active');
+    NoTitle.classList.toggle('no-title');
 })
